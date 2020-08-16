7a24885dd9ad042b962ce34fc39ef5c20ac9f8b8
Mihai Dinculescu <mihai.dinculescu@outlook.com>
2020-08-16 18:37:46 +0100

    Command tests

--- src/tests/command.rs
+++ src/tests/command.rs
@@ -34,11 +34,37 @@ fn folder_name_string_to_enum(folder_name_string: &str, folder_name_enum: Folder
     },
 )]
 fn folder_name_string_to_enum_error(folder_name_string: &str) {
-    let err = FolderNameEnum::from_str(folder_name_string).err().unwrap();
+    let result = FolderNameEnum::from_str(folder_name_string);
+    let err = result.err().unwrap();
 
     assert_eq!(err.kind(), io::ErrorKind::InvalidInput);
     assert_eq!(
         err.to_string(),
         "Valid options are: rust | target | node | node_modules"
     );
+}
+
+#[parameterized(
+    folder_name_enum = {
+        FolderNameEnum::NodeModules,
+        FolderNameEnum::Target,
+    },
+    folder_name_string = {
+        "node_modules",
+        "target",
+    },
+)]
+fn folder_name_enum_to_string(folder_name_enum: FolderNameEnum, folder_name_string: &str) {
+    assert_eq!(folder_name_enum.to_string(), folder_name_string);
+}
+
+#[parameterized(
+    folder_name_enum = {
+        FolderNameEnum::Node,
+        FolderNameEnum::Rust,
+    },
+)]
+#[should_panic]
+fn folder_name_enum_to_string_panic(folder_name_enum: FolderNameEnum) {
+    let _ = folder_name_enum.to_string();
 }
